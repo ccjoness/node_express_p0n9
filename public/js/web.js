eb06e528a46fa04343c2b212b8781e8adde45f55
Chris Jones <chris.charles.jones@gmail.com>
2018-09-19 21:29:55 -0700

    trying to get ssl and socket.io to play nice

--- public/js/web.js
+++ public/js/web.js
@@ -1,7 +1,7 @@
 //
 
 
-const socket = io.connect('ws://www.thep0n9.com', {secure: true});
+const socket = io.connect('https://www.thep0n9.com', {secure: true});
 
 socket.on('games_list', function (data) {
     $('#gameTable').html('');
